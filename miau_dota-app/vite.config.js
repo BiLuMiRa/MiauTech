71d40cf3b37cff5957eeaefc75c45acad6a60dcb
ifpb <ifpb@DSK11LB02-JP>
2025-11-05 17:28:15 -0300

    configuracao do vite.config.js

--- miau_dota-app/vite.config.js
+++ miau_dota-app/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/miau-dota/',
+    base: '/Miau-dota/',
     build: {
         rollupOptions: {
             input: {
