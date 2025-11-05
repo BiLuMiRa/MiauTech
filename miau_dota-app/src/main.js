b9a1d2aa1ed194c7914806e73bef42c88fc04e6e
ifpb <ifpb@DSK11LB02-JP>
2025-11-05 16:55:57 -0300

    .io

---
+++ miau_dota-app/src/main.js
@@ -0,0 +1,20 @@
+import './style.css'
+import pets from './pets'
+
+function createPet(pets){
+    return `
+    <div>
+        <img src="${pets.image}">
+        <p class="name">${pets.name}</p>
+        <p class="infos-pet">${pets.age} | ${pets.sexo}</p>
+        <img src="src/imgs/Itens miaudota/pata1.png" alt="pata1" id="pata1">
+    </div>`
+           
+}
+
+const petcard = document.querySelector('.pets');
+
+pets.forEach((pets) => {
+    let card = createPet(pets);
+    petcard.insertAdjacentHTML('beforeend', card);
+});
