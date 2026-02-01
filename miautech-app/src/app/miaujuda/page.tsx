16ca830b1f80ad3cda745aff459e6b9cf18d6f40
Raquel <anaraquelbpereira@gmail.com>
2026-02-01 16:44:56 -0300

    Fix: Corrige erro no deploy

--- miautech-app/src/app/miaujuda/page.tsx
+++ miautech-app/src/app/miaujuda/page.tsx
@@ -4,9 +4,9 @@ import { supabase } from "../../lib/supabase";
 import React from "react";
 import { useState, useEffect } from "react";
 import "./style.css";
-import Vets from "@/src/components/miaujuda/Vets";
-import Busca from "@/src/components/miaujuda/Busca";
-import VetsBuscados from "@/src/components/miaujuda/VetsBuscados";
+import Vets from "../../components/miaujuda/Vets";
+import Busca from "../../components/miaujuda/Busca";
+import VetsBuscados from "../../components/miaujuda/VetsBuscados";
 
 export default function Home() {
   const [busca, setBusca] = useState(false);
